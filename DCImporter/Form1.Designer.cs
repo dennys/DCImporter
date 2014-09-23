dd297baacac6baabbdb119a363cda870c17e996e
Dennys <dennys.hsieh@gmail.com>
2014-09-23 22:49:38 +0800

    initial

---
+++ DCImporter/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace DCImporter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPhotoDirectory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxLastFilename = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(186, 6);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(66, 26);
+            this.buttonRead.TabIndex = 0;
+            this.buttonRead.Text = "&Read";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "RX100",
+            "G6"});
+            this.comboBox1.Location = new System.Drawing.Point(38, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(129, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxPhotoDirectory);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxLastFilename);
+            this.groupBox1.Location = new System.Drawing.Point(38, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(299, 339);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Photo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Directory:";
+            // 
+            // textBoxPhotoDirectory
+            // 
+            this.textBoxPhotoDirectory.Location = new System.Drawing.Point(58, 19);
+            this.textBoxPhotoDirectory.Name = "textBoxPhotoDirectory";
+            this.textBoxPhotoDirectory.Size = new System.Drawing.Size(194, 22);
+            this.textBoxPhotoDirectory.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Last file:";
+            // 
+            // textBoxLastFilename
+            // 
+            this.textBoxLastFilename.Location = new System.Drawing.Point(58, 47);
+            this.textBoxLastFilename.Name = "textBoxLastFilename";
+            this.textBoxLastFilename.Size = new System.Drawing.Size(194, 22);
+            this.textBoxLastFilename.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(58, 100);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(235, 220);
+            this.listBox1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Files:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(458, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 26);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "&Copy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.progressBar1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 412);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(847, 29);
+            this.flowLayoutPanel1.TabIndex = 9;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(3, 3);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(586, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 441);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonRead);
+            this.Name = "Form1";
+            this.Text = "DCImporter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxPhotoDirectory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxLastFilename;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ DCImporter/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DCImporter
+{
+
+    //sss
+    
+    public partial class Form1 : Form
+    {
+
+        IniData parsedData;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileIniDataParser fileIniData = new FileIniDataParser();
+            ;
+            if (File.Exists(Directory.GetCurrentDirectory() + "\\DCImporter.ini"))
+            {
+                parsedData = fileIniData.ReadFile(Directory.GetCurrentDirectory() + "\\DCImporter.ini");
+                string photoDirectory = parsedData["RX100"]["PHOTO_DIRECTORY"];
+                string lastFilename = parsedData["RX100"]["LASTFILE"];
+
+                textBoxPhotoDirectory.Text = photoDirectory;
+                textBoxLastFilename.Text = lastFilename;
+
+                string[] filePaths = Directory.GetFiles(@photoDirectory, "*.jpg", SearchOption.AllDirectories);
+                Boolean start = false;
+                foreach (string i in filePaths) {
+                    if (i.IndexOf(lastFilename) != -1)
+                    {
+                        start = true;
+                    }
+                    if (start == true) {
+                        listBox1.Items.Add(i);
+                    }
+                }
+                listBox1.Items.RemoveAt(0); // Remove 1st item
+
+            }
+
+        }
+
+        private void textBoxPhotoDirectory_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string targetDirectory = parsedData["RX100"]["TARGET_DIRECTORY"];
+
+            if (!System.IO.Directory.Exists(targetDirectory))
+            {
+                System.IO.Directory.CreateDirectory(targetDirectory);
+            }
+
+            progressBar1.Maximum = listBox1.Items.Count;
+            progressBar1.Value = 0;
+            foreach (string i in listBox1.Items)
+            {
+                string filename = Path.GetFileName(i);
+                string targetFile = System.IO.Path.Combine(targetDirectory, filename);
+                //System.IO.File.Copy(i, targetFile); // not overwrite
+                System.IO.File.Copy(i, targetFile, true);  // overwrite
+                progressBar1.Value += 1;
+            }
+
+            string newLastFilename = Path.GetFileName(listBox1.Items[listBox1.Items.Count - 1].ToString());
+
+            MessageBox.Show("Processing was completed");
+            progressBar1.Value = 0;
+        }
+    }
+}
