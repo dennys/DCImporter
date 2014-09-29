c608f90090774983d87b9101a6498b04a573b7f7
Dennys <dennys.hsieh@gmail.com>
2014-09-29 23:35:35 +0800

    fix #1

--- DCImporter/Form1.Designer.cs
+++ DCImporter/Form1.Designer.cs
@@ -28,16 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonRead = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBoxPhotoDirectory = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBoxLastFilename = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
@@ -45,114 +35,36 @@
             this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
-            this.groupBox1.SuspendLayout();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPhotoDirectory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxLastFilename = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.flowLayoutPanel1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // buttonRead
-            // 
-            this.buttonRead.Location = new System.Drawing.Point(271, 37);
-            this.buttonRead.Name = "buttonRead";
-            this.buttonRead.Size = new System.Drawing.Size(66, 26);
-            this.buttonRead.TabIndex = 0;
-            this.buttonRead.Text = "&Read";
-            this.buttonRead.UseVisualStyleBackColor = true;
-            this.buttonRead.Click += new System.EventHandler(this.btnRead_Click);
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "RX100",
-            "G6"});
-            this.comboBox1.Location = new System.Drawing.Point(96, 37);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(129, 20);
-            this.comboBox1.TabIndex = 2;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.listBox1);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.textBoxPhotoDirectory);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.textBoxLastFilename);
-            this.groupBox1.Location = new System.Drawing.Point(38, 63);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(299, 339);
-            this.groupBox1.TabIndex = 6;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Photo";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 100);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(29, 12);
-            this.label3.TabIndex = 11;
-            this.label3.Text = "Files:";
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(58, 100);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(235, 220);
-            this.listBox1.TabIndex = 10;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 12);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Directory:";
-            // 
-            // textBoxPhotoDirectory
-            // 
-            this.textBoxPhotoDirectory.Location = new System.Drawing.Point(58, 19);
-            this.textBoxPhotoDirectory.Name = "textBoxPhotoDirectory";
-            this.textBoxPhotoDirectory.Size = new System.Drawing.Size(194, 22);
-            this.textBoxPhotoDirectory.TabIndex = 8;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 47);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 12);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Last file:";
-            // 
-            // textBoxLastFilename
-            // 
-            this.textBoxLastFilename.Location = new System.Drawing.Point(58, 47);
-            this.textBoxLastFilename.Name = "textBoxLastFilename";
-            this.textBoxLastFilename.Size = new System.Drawing.Size(194, 22);
-            this.textBoxLastFilename.TabIndex = 6;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(458, 31);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(66, 26);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "&Copy";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.btnCopy_Click);
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.AutoSize = true;
             this.flowLayoutPanel1.Controls.Add(this.progressBar1);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 412);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 543);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(847, 29);
             this.flowLayoutPanel1.TabIndex = 9;
@@ -187,51 +99,189 @@
             // optionsToolStripMenuItem
             // 
             this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
-            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
             this.optionsToolStripMenuItem.Text = "&Options";
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(123, 6);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
             this.exitToolStripMenuItem.Text = "E&xit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.buttonRead);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(847, 41);
+            this.panel1.TabIndex = 12;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(50, 38);
+            this.label4.Location = new System.Drawing.Point(17, 14);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(39, 12);
-            this.label4.TabIndex = 11;
+            this.label4.TabIndex = 12;
             this.label4.Text = "Profile:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "RX100",
+            "G6"});
+            this.comboBox1.Location = new System.Drawing.Point(60, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(129, 20);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(209, 7);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(66, 26);
+            this.buttonRead.TabIndex = 14;
+            this.buttonRead.Text = "&Read";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(287, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 26);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "&Copy";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 65);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(404, 478);
+            this.panel2.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel4);
+            this.groupBox1.Controls.Add(this.panel3);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(404, 478);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Photo";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.textBoxPhotoDirectory);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.textBoxLastFilename);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(3, 18);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(398, 93);
+            this.panel3.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 12);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Directory:";
+            // 
+            // textBoxPhotoDirectory
+            // 
+            this.textBoxPhotoDirectory.Location = new System.Drawing.Point(54, 12);
+            this.textBoxPhotoDirectory.Name = "textBoxPhotoDirectory";
+            this.textBoxPhotoDirectory.Size = new System.Drawing.Size(194, 22);
+            this.textBoxPhotoDirectory.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Last file:";
+            // 
+            // textBoxLastFilename
+            // 
+            this.textBoxLastFilename.Location = new System.Drawing.Point(54, 40);
+            this.textBoxLastFilename.Name = "textBoxLastFilename";
+            this.textBoxLastFilename.Size = new System.Drawing.Size(194, 22);
+            this.textBoxLastFilename.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Files:";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.listBox1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(3, 111);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(398, 364);
+            this.panel4.TabIndex = 13;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(398, 364);
+            this.listBox1.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(847, 441);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(847, 572);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.flowLayoutPanel1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.buttonRead);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "DCImporter";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.flowLayoutPanel1.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -239,16 +289,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button buttonRead;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBoxPhotoDirectory;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBoxLastFilename;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.MenuStrip menuStrip1;
@@ -256,7 +296,21 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxPhotoDirectory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxLastFilename;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- DCImporter/Form1.cs
+++ DCImporter/Form1.cs
@@ -26,11 +26,42 @@ namespace DCImporter
             InitializeComponent();
         }
 
-        private void btnRead_Click(object sender, EventArgs e)
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            string targetDirectory = iniGlobal["RX100"]["TARGET_DIRECTORY"];
+
+            if (!System.IO.Directory.Exists(targetDirectory))
+            {
+                System.IO.Directory.CreateDirectory(targetDirectory);
+            }
+
+            progressBar1.Maximum = listBox1.Items.Count;
+            progressBar1.Value = 0;
+            foreach (string i in listBox1.Items)
+            {
+                string filename = Path.GetFileName(i);
+                string targetFile = System.IO.Path.Combine(targetDirectory, filename);
+                //System.IO.File.Copy(i, targetFile); // not overwrite
+                System.IO.File.Copy(i, targetFile, true);  // overwrite
+                progressBar1.Value += 1;
+            }
+
+            string newLastFilename = Path.GetFileName(listBox1.Items[listBox1.Items.Count - 1].ToString());
+
+            MessageBox.Show("Processing was completed");
+            progressBar1.Value = 0;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
         {
             FileIniDataParser fileIniData = new FileIniDataParser();
             string diskId = null;
-            
+
             if (File.Exists(Directory.GetCurrentDirectory() + "\\DCImporter.ini"))
             {
                 iniGlobal = fileIniData.ReadFile(Directory.GetCurrentDirectory() + "\\DCImporter.ini");
@@ -67,12 +98,14 @@ namespace DCImporter
 
                 string[] filePaths = Directory.GetFiles(@photoDirectory, "*.jpg", SearchOption.AllDirectories);
                 Boolean start = false;
-                foreach (string i in filePaths) {
+                foreach (string i in filePaths)
+                {
                     if (i.IndexOf(lastFilename) != -1)
                     {
                         start = true;
                     }
-                    if (start == true) {
+                    if (start == true)
+                    {
                         listBox1.Items.Add(i);
                     }
                 }
@@ -85,43 +118,6 @@ namespace DCImporter
             }
 
             Guid id = Guid.NewGuid();
-
-        }
-
-        private void textBoxPhotoDirectory_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnCopy_Click(object sender, EventArgs e)
-        {
-            string targetDirectory = iniGlobal["RX100"]["TARGET_DIRECTORY"];
-
-            if (!System.IO.Directory.Exists(targetDirectory))
-            {
-                System.IO.Directory.CreateDirectory(targetDirectory);
-            }
-
-            progressBar1.Maximum = listBox1.Items.Count;
-            progressBar1.Value = 0;
-            foreach (string i in listBox1.Items)
-            {
-                string filename = Path.GetFileName(i);
-                string targetFile = System.IO.Path.Combine(targetDirectory, filename);
-                //System.IO.File.Copy(i, targetFile); // not overwrite
-                System.IO.File.Copy(i, targetFile, true);  // overwrite
-                progressBar1.Value += 1;
-            }
-
-            string newLastFilename = Path.GetFileName(listBox1.Items[listBox1.Items.Count - 1].ToString());
-
-            MessageBox.Show("Processing was completed");
-            progressBar1.Value = 0;
-        }
-
-        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
         }
     }
 }
